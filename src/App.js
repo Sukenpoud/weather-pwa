d74427db9c9fb5c15383b3fae2181ab4ea8d4340
Romain <axilais.romain@gmail.com>
2022-11-25 15:30:01 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test';
 
 function App() {
+
+  const [Getter, Setter] = React.useState(1);
+
+  console.log(Getter);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="" onClick={() => Setter(Getter+1)}>
+      <Test title='Oui le test'></Test>
+      {Getter}
     </div>
   );
 }
